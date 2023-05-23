768d2132b5e1684ea41b7f5328a6d24290ffaa65
Cimorexave <sadeq.s502@gmail.com>
2023-05-23 20:23:54 +0330

    html

--- index.php
+++ index.php
@@ -1,2 +1,30 @@
-<?php
-echo "hi world";
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-sandbox</title>
+</head>
+
+<body>
+    <?php
+    echo "click this button!" . "<br>";
+    ?>
+    <form action="form.php">
+        <button>Submit</button>
+    </form>
+
+    <style>
+        button {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            padding: 2px 5px;
+            border-radius: 50%;
+        }
+    </style>
+</body>
+
+</html>
