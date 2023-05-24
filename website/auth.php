de4b37da6d331795a0f849e70ef23da0854ab9cc
Cimorexave <sadeq.s502@gmail.com>
2023-05-24 21:36:51 +0330

    empty instead of isset

--- website/auth.php
+++ website/auth.php
@@ -1,5 +1,5 @@
 <?php
 session_start();
-if (!isset($_SESSION["username"]) && !isset($_SESSION["password"])) {
+if (empty($_SESSION["username"]) && empty($_SESSION["password"])) {
     header("Location: ./login.php");
 }
