f968360d4aecfc077578b68d9ce9dc5daa534bb9
Cimorexave <sadeq.s502@gmail.com>
2023-05-24 17:42:26 +0330

    update task effort

--- index.php
+++ index.php
@@ -16,16 +16,40 @@
         <button>Submit</button>
         <input type="text" name="yourname">
     </form>
-
+    <form action="index.php" method="post">
+        <div>
+            <label for="task">your task</label>
+            <input type="text" id="task" name="task">
+        </div>
+        <input type="submit" name="create" value="Create New Tasl">
+    </form>
     <style>
         button {
             display: flex;
             align-items: center;
             justify-content: center;
             padding: 2px 5px;
-            border-radius: 50%;
+            border-radius: 1rem;
+        }
+
+        div {
+            display: flex;
+            gap: 1rem;
         }
     </style>
 </body>
 
 </html>
+
+<?php
+
+if (isset($_POST["create"])) {
+    $my_tasks = array();
+    array_push($my_tasks, $_POST["task"]);
+}
+
+
+foreach ($my_tasks as $task) {
+    echo "task";
+}
+?>
