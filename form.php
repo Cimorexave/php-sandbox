0de0e9688bf747a63d3d9db32c23503288b36d7e
Cimorexave <sadeq.s502@gmail.com>
2023-05-24 17:01:43 +0330

    from submission

--- form.php
+++ form.php
@@ -1,2 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="form.php" method="post">
+        <div class="field">
+            <label for="firstName">First Name</label>
+            <input type="text" id="firstName" name="firstName">
+        </div>
+        <div class="field">
+            <label for="lastName">Last Name</label>
+            <input type="text" id="lastName" name="lastName">
+        </div>
+        <input type="submit" value="Submit" name="submit">
+    </form>
+    </div>
+</body>
+
+</html>
 <?php
-echo "Congrats {$_REQUEST["yourname"]}, you clicked a button!";
+// foreach ($_POST as $key => $value) {
+//     echo "$key => $value <br>";
+// }
+if (isset($_POST["submit"])) {
+    $firstname = $_POST["firstName"];
+    $lastname = $_POST["lastName"];
+    if (!empty($_POST["firstName"]) && !empty($_POST["lastName"])) {
+        echo "Your full name is $firstname $lastname";
+    } else echo "Username or Passowrd are missing.";
+}
+?>
