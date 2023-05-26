1b475f55937c5700697b4f47228f77cfc850148b
Cimorexave <sadeq.s502@gmail.com>
2023-05-26 13:55:12 +0330

    connections definition

--- oop/database/connection.php
+++ oop/database/connection.php
@@ -1,4 +1,21 @@
 <?php
 
+// Modules
 require "./config.php";
-new DB_Config("mydb", "localhost", "root");
+
+// Definitions
+class DB_Connection
+{
+    public mysqli $connection;
+    public function __construct(DB_Config $config)
+    {
+        $this->connection = mysqli_connect($config->host, $config->user, $config->pass, $config->name);
+    }
+    public function stop()
+    {
+        mysqli_close($this->connection);
+    }
+}
+
+$my_config = new DB_Config("mydb", "localhost", "root");
+$my_connection = new DB_Connection($my_config);
